62bcdef323671204d0c9cb5c903ad959674fad3b
Tharaka <wtharakaherath@gmail.com>
2026-04-17 22:38:13 +0530

    feat(urls): add authentication URLs for register, login, logout

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.home, name='home'),
     path('showtimes/<int:movie_id>/', views.showtimes, name='showtimes'),
+    
+    # Authentication URLs 
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
