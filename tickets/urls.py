c1cf959c9532cf1ae9979415d5dba058555f6962
Work105 <workofficial105@gmail.com>
2026-04-17 12:32:51 +0530

    Update urls.py

--- tickets/urls.py
+++ tickets/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
 urlpatterns = [
     path('download-ticket/<int:booking_id>/', views.download_ticket, name='download_ticket'),
 ]
+
+urlpatterns = [
+path('cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),]
