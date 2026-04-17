b508e65b5fa73f3c579f92cafc0249461a6958f0
Tharaka <wtharakaherath@gmail.com>
2026-04-17 21:23:20 +0530

    fix(settings): add login URL config and fix template directories

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'movie_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],  # Change from 'templates' to [] since templates are in app
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
