d43fbfe067df35c85794320e2de99f43350ea132
Work105 <workofficial105@gmail.com>
2026-04-17 12:31:22 +0530

    Update urls.py

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path
+from . import views
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.test_view, name='test'),
+]
+
+
+urlpatterns = [
+    path('download-ticket/<int:booking_id>/', views.download_ticket, name='download_ticket'),
+]
