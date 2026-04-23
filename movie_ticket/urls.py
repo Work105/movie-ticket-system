a910a601fd0dea07d0d467dc16503af603bb5a58
Tharaka <wtharakaherath@gmail.com>
2026-04-23 21:24:30 +0530

    Add clean settings.py and urls.py for movie_ticket

--- movie_ticket/urls.py
+++ movie_ticket/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tickets.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
