62a665b8b762a1de5326d47645b0fbe1db82e78d
Tharaka <wtharakaherath@gmail.com>
2026-04-24 16:38:34 +0530

    feat: Register CustomUser model in admin panel to view registered users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
